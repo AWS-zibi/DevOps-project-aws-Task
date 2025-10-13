d92c5c0d56bc7147efe0c49c3b0ec7ea14773a37
SHABARI US <awszibi@gmail.com>
2025-10-13 17:40:59 +0530

    Update server.js

--- app/server.js
+++ app/server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.end('Hello World from AWS EKS using Jenkins CI/CD!');
+  res.end('Hello, Kube World! – a fun twist on “Hello World”');
 });
 
 server.listen(port, () => {
