264e0ebbfd7cf71b269bd69d62adf3f00995b826
SHABARI US <awszibi@gmail.com>
2025-10-14 11:33:44 +0530

    Update server.js

--- app/server.js
+++ app/server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.end('Hello, "Kube World!" – a fun twist on “Cloudops-World ...!{"}');
+  res.end('Hello, "Kube World!" – a fun twist on “Cloudops-World ...!{🚀🚀🚀}');
 });
 
 server.listen(port, () => {
